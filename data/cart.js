7a476b9931223af26b0c95db265e96fa8b99add1
IB-ALA <ishaqibrahimyusif@gmail.com>
2024-03-21 13:49:34 +0000

    Save cart in localStorage.

--- data/cart.js
+++ data/cart.js
@@ -1,13 +1,19 @@
-export let cart = [
-  {
-    productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-    quantity: 2
-  },
-  {
-    productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-    quantity: 1
-  }
-];
+export let cart = JSON.parse(localStorage.getItem('cart'));
+// if cart if empty, localStorage will give NULL
+// !NULL = true;
+
+if (!cart) {
+  cart = [
+    {
+      productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+      quantity: 2
+    },
+    {
+      productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+      quantity: 1
+    }
+  ];
+}
 
 export function addToCart(productId) {
   const selectorElem = document.querySelector(`.js-quantity-selector-${productId}`);
@@ -29,7 +35,15 @@ export function addToCart(productId) {
       quantity
     });
   }
+
+  saveToStorage();
 }
+
+
+function saveToStorage() {
+  localStorage.setItem('cart', JSON.stringify(cart));
+}
+
 
 export function removeFromCart(productId) {
 
@@ -46,4 +60,5 @@ export function removeFromCart(productId) {
   */
 
   cart = cart.filter((cartItem) => cartItem.productId !== productId);
+  saveToStorage();
 }
