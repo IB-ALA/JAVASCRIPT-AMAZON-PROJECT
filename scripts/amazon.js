47f581b2beb15701c02feda03d581333d56f84a6
IB-ALA <ishaqibrahimyusif@gmail.com>
2024-03-21 12:47:07 +0000

    Fix price fomatting and radio selectors.

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,5 +1,6 @@
 import {cart, addToCart} from '../data/cart.js';
 import {products} from '../data/products.js';
+import {formatCurrency} from './utils/money.js';
 
 const productsGrid = document.querySelector('.js-products-grid');
 const caryQuantityElem = document.querySelector('.js-cart-quantity');
@@ -30,7 +31,7 @@ products.forEach((product) => {
       </div>
 
       <p class="product-price">
-        $${(product.priceCents / 100).toFixed(2)}
+        $${formatCurrency(product.priceCents)}
       </p>
 
       <div class="product-quantity-container">
