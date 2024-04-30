4f0647ca70997cad8105e43ef43bcf50329776aa
IB-ALA <ishaqibrahimyusif@gmail.com>
2024-04-30 13:43:42 +0000

    Learn more details about classes and this.

--- data/products.js
+++ data/products.js
@@ -63,6 +63,39 @@ class Clothing extends Product {
   }
 }
 
+/*
+const date = new Date();
+console.log(date);
+console.log(date.toLocaleTimeString());
+*/
+
+console.log(this);
+
+/*
+const object2 = {
+  a: 2,
+  b: this.a
+}
+console.log(object2.b);
+// YOU CAN USE this WHEN YOU'RE INSIDE A FUNCTION OR METHOD.
+*/
+
+/*
+function logThis() {
+  console.log(this);
+}
+
+logThis();
+logThis.call('hello');
+
+const object3 = {
+  method: () => {
+    console.log(this);
+  }
+}
+object3.method();
+*/
+
 
 export const products = [
   {
