7e0cb6369284ec51a3fc3ddd2ee69decc641862f
IB-ALA <ishaqibrahimyusif@gmail.com>
2024-05-02 13:13:05 +0000

    Exercise 17i. (Creating test cases for product.js)

--- data/products.js
+++ data/products.js
@@ -15,7 +15,7 @@ export function getProduct(productId) {
 }
 
 
-class Product {
+export class Product {
   id;
   image;
   name;
@@ -44,7 +44,7 @@ class Product {
 }
 
 
-class Clothing extends Product {
+export class Clothing extends Product {
   sizeChartLink;
 
   constructor(productDetails) {
@@ -64,7 +64,7 @@ class Clothing extends Product {
 }
 
 
-class Appliance extends Product {
+export class Appliance extends Product {
   instructionsLink;
   warrantyLink;
 
---
+++ tests/data/productsTest.js
@@ -0,0 +1,161 @@
+
+
+import { Product, Clothing, Appliance } from '../../data/products.js';
+
+
+describe('test suite: Product, Clothing and Appliance class', () => {
+
+  it('creates a new Product', () => {
+    const product1 = new Product(
+      {
+        id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+        image: "images/products/athletic-cotton-socks-6-pairs.jpg",
+        name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
+        rating: {
+          stars: 4.5,
+          count: 87
+        },
+        priceCents: 1090,
+        keywords: [
+          "socks",
+          "sports",
+          "apparel"
+        ]
+      }
+    );
+
+    // product1.getPrice();
+
+    expect(product1.getPrice()).toEqual('$10.90');
+
+    expect(product1.getStarsUrl()).toEqual(`images/ratings/rating-${product1.rating.stars * 10}.png`);
+
+    expect(product1.extraInfoHTML()).not.toContain('Size chart');
+  });
+
+  it('creates a new Clothing without the clothing value', () => {
+    const product1 = new Clothing(
+      {
+        id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+        image: "images/products/athletic-cotton-socks-6-pairs.jpg",
+        name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
+        rating: {
+          stars: 4.5,
+          count: 87
+        },
+        priceCents: 1090,
+        keywords: [
+          "socks",
+          "sports",
+          "apparel"
+        ]
+      }
+    );
+
+    expect(product1.getPrice()).toEqual('$10.90');
+
+    expect(product1.getStarsUrl()).toEqual(`images/ratings/rating-${product1.rating.stars * 10}.png`);
+
+    expect(product1.extraInfoHTML()).toContain('undefined');
+  });
+
+  it('creates a new Clothing with the clothing value', () => {
+    const product1 = new Clothing(
+      {
+        id: "83d4ca15-0f35-48f5-b7a3-1ea210004f2e",
+        image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
+        name: "Adults Plain Cotton T-Shirt - 2 Pack",
+        rating: {
+          stars: 4.5,
+          count: 56
+        },
+        priceCents: 799,
+        keywords: [
+          "tshirts",
+          "apparel",
+          "mens"
+        ],
+        type: "clothing",
+        sizeChartLink: "images/clothing-size-chart.png"
+      }
+    );
+
+    expect(product1.getPrice()).toEqual('$7.99');
+
+    expect(product1.getStarsUrl()).toEqual(`images/ratings/rating-${product1.rating.stars * 10}.png`);
+
+    expect(product1.extraInfoHTML()).not.toContain('undefined');
+
+    expect(product1.extraInfoHTML()).toContain('images/clothing-size-chart.png');
+  });
+
+  it('creates a new Appliance without the appliance value', () => {
+    const product1 = new Appliance(
+      {
+        id: "83d4ca15-0f35-48f5-b7a3-1ea210004f2e",
+        image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
+        name: "Adults Plain Cotton T-Shirt - 2 Pack",
+        rating: {
+          stars: 4.5,
+          count: 56
+        },
+        priceCents: 799,
+        keywords: [
+          "tshirts",
+          "apparel",
+          "mens"
+        ],
+        type: "clothing",
+        sizeChartLink: "images/clothing-size-chart.png"
+      }
+    );
+
+    expect(product1.getPrice()).toEqual('$7.99');
+
+    expect(product1.getStarsUrl()).toEqual(`images/ratings/rating-${product1.rating.stars * 10}.png`);
+
+    expect(product1.extraInfoHTML()).toContain('undefined');
+
+    expect(product1.extraInfoHTML()).not.toContain('images/appliance-warranty.png');
+
+    expect(product1.extraInfoHTML()).toContain('Warranty');
+  });
+
+  it('creates a new Appliance with the appliance value', () => {
+    const product1 = new Appliance(
+      {
+        id: "54e0eccd-8f36-462b-b68a-8182611d9add",
+        image: "images/products/black-2-slot-toaster.jpg",
+        name: "2 Slot Toaster - Black",
+        rating: {
+          stars: 5,
+          count: 2197
+        },
+        priceCents: 1899,
+        keywords: [
+          "toaster",
+          "kitchen",
+          "appliances"
+        ],
+        type: "appliance",
+        instructionsLink: "images/appliance-instructions.png",
+        warrantyLink: "images/appliance-warranty.png"
+      }
+    );
+
+    expect(product1.getPrice()).toEqual('$18.99');
+
+    expect(product1.getStarsUrl()).toEqual(`images/ratings/rating-${product1.rating.stars * 10}.png`);
+
+    expect(product1.getStarsUrl()).toEqual(`images/ratings/rating-50.png`);
+
+    expect(product1.extraInfoHTML()).not.toContain('undefined');
+
+    expect(product1.extraInfoHTML()).toContain('images/appliance-warranty.png');
+
+    expect(product1.extraInfoHTML()).toContain('Warranty');
+
+    expect(product1.extraInfoHTML()).toContain('Instructions');
+  });
+
+})
