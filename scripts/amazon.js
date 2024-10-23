18f4c5cd305c9fbd88ab20d5dd1c76927ee4a552
IB-ALA <ishaqibrahimyusif@gmail.com>
2024-10-23 17:39:00 +0000

    Searchbar functionality done.

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,15 +1,28 @@
 // import {cart, addToCart, calculateQuantity} from '../data/cart.js';
-import { cart } from '../data/cart-class.js';
-import { products, loadProductsFetch } from '../data/products.js';
+import { cart } from "../data/cart-class.js";
+import { products, loadProductsFetch } from "../data/products.js";
 // import {formatCurrency} from './utils/money.js';
 
-const productsGrid = document.querySelector('.js-products-grid');
-const caryQuantityElem = document.querySelector('.js-cart-quantity');
+const productsGrid = document.querySelector(".js-products-grid");
+const caryQuantityElem = document.querySelector(".js-cart-quantity");
 
-document.querySelector('.js-search-button').addEventListener('click', () => {
-  const productsToSearch = document.querySelector('.js-search-bar').value;
-  window.location.href = `amazon.html?search=${productsToSearch}`
+document
+  .querySelector(".js-search-button")
+  .addEventListener("click", loadSearchedProducts);
+document.querySelector(".js-search-bar").addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    loadSearchedProducts();
+  }
 });
+
+function loadSearchedProducts() {
+  const productsToSearch = document
+    .querySelector(".js-search-bar")
+    .value.trim();
+  if (productsToSearch) {
+    window.location.href = `amazon.html?search=${productsToSearch}`;
+  }
+}
 
 try {
   await loadProductsFetch();
@@ -26,7 +39,7 @@ updateCartQuantity();
 // console.log(products);
 
 function renderProductsGrid(products) {
-  let productsHTML = '';
+  let productsHTML = "";
 
   products.forEach((product) => {
     productsHTML += `
@@ -76,7 +89,9 @@ function renderProductsGrid(products) {
           Added
         </div>
 
-        <button class="add-to-cart-button button-primary js-add-to-cart-button" data-product-id="${product.id}">
+        <button class="add-to-cart-button button-primary js-add-to-cart-button" data-product-id="${
+          product.id
+        }">
           Add to Cart
         </button>
       </section>
@@ -85,10 +100,9 @@ function renderProductsGrid(products) {
 
   productsGrid.innerHTML = productsHTML;
 
-
-  const addToCartBtn = document.querySelectorAll('.js-add-to-cart-button');
+  const addToCartBtn = document.querySelectorAll(".js-add-to-cart-button");
   addToCartBtn.forEach((button) => {
-    button.addEventListener('click', () => {
+    button.addEventListener("click", () => {
       const { productId } = button.dataset;
 
       cart.addToCart(productId);
@@ -98,44 +112,47 @@ function renderProductsGrid(products) {
   });
 }
 
-
 function loadPage() {
-
   // CHECK FOR SEARCH PARAMS.
   const url = new URL(window.location.href);
-  const productsToSearch = url.searchParams.get('search');
+  const productsToSearch = url.searchParams.get("search");
   if (productsToSearch) {
-    console.log('yes');
+    // console.log("yes");
+    document.querySelector(".js-search-bar").value = productsToSearch;
     const newProducts = products.filter((product) => {
-      return product.name.includes(productsToSearch);
+      // check if it works
+      return (
+        product.name.toLowerCase().includes(productsToSearch.toLowerCase()) ||
+        product.keywords.forEach((keyword) => {
+          keyword.toLowerCase().includes(productsToSearch.toLowerCase());
+        })
+      );
     });
     // console.log(newProducts);
-    
+
     if (newProducts.length > 0) {
       renderProductsGrid(newProducts);
     } else {
-      productsGrid.innerHTML =  `<p style="width: 100%; text-align: center;">No Products Found For '${productsToSearch}'</p>`
+      productsGrid.innerHTML = `<p style="width: 100%; text-align: center;">No Products Found For '${productsToSearch}'</p>`;
     }
-  
   } else {
-    console.log('no');
+    // console.log("no");
     renderProductsGrid(products);
   }
-  
 }
-
-
 
 function updateCartQuantity() {
   caryQuantityElem.innerHTML = cart.calculateQuantity();
 }
 
 function displayAddedText(productId) {
-  const addedTextElem = document.querySelector(`.js-added-to-cart-${productId}`);
+  const addedTextElem = document.querySelector(
+    `.js-added-to-cart-${productId}`
+  );
 
-  addedTextElem.classList.add('make-text-visible');
+  addedTextElem.classList.add("make-text-visible");
   clearTimeout(timeoutId);
   timeoutId = setTimeout(() => {
-    addedTextElem.classList.remove('make-text-visible');
+    addedTextElem.classList.remove("make-text-visible");
   }, 2000);
 }
